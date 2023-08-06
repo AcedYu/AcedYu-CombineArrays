829d0e0f9f2fe4834f5cae3ad8b276fd546be32b
Alexander Yu <alexyu299@hotmail.com>
2023-08-06 04:28:54 +0000

    lab complete

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 public class CombineArrays {
     /**
      * Combine two arrays into a single array and return it.
@@ -8,7 +8,10 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int length = arr1.length + arr2.length;
+        int[]result = new int[length];
+        System.arraycopy(arr1, 0, result, 0, arr1.length);
+        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
+        return result;
     }
 }
